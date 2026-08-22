e7716d32037533afa36e9dfcbe935e0c2d5b33aa
Saishh34 <saishkenekar29k@gmail.com>
2026-08-22 15:36:00 +0530

    Add surveillance API

---
+++ api.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.get("/api/health")
+def health():
+    return jsonify({
+        "status": "online"
+    })
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000, debug=True)
